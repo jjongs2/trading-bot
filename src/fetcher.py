8e4ca636cd45aba5b9ad4dbfd164e4afdcb52084
Ki-jong Song <jjongs212@gmail.com>
2024-07-29 19:43:03 +0900

    Reorder methods in Fetcher class

--- src/fetcher.py
+++ src/fetcher.py
@@ -27,29 +27,6 @@ class Fetcher:
             logger.error(f'Failed to fetch symbol information: {e}')
             raise
 
-    def fetch_position(self, symbol_info: dict, my_position: Position) -> None:
-        logger.info('Fetching position information...')
-        try:
-            positions = self._exchange.fetch_positions(symbols=[self._symbol])
-            if not positions:
-                my_position.update(None)
-                self._exchange.cancel_all_orders(self._symbol)
-                logger.info(f"No active position for {symbol_info['id']}")
-                return
-            position = positions[0]
-            amount = position['contracts']
-            entry_price = position['entryPrice']
-            side = Side(position['side'])
-            my_position.update(side, amount, entry_price)
-            logger.info(
-                f"Current position:"
-                f" {amount * side.sign()} {symbol_info['base']}"
-                f" at {entry_price} {symbol_info['quote']}"
-            )
-        except Exception as e:
-            logger.error(f'Failed to fetch position: {e}')
-            raise
-
     def fetch_historical_data(
         self,
         interval: str,
@@ -79,6 +56,29 @@ class Fetcher:
             return close_data
         except Exception as e:
             logger.error(f'Failed to fetch historical data: {e}')
+            raise
+
+    def fetch_position(self, symbol_info: dict, my_position: Position) -> None:
+        logger.info('Fetching position information...')
+        try:
+            positions = self._exchange.fetch_positions(symbols=[self._symbol])
+            if not positions:
+                my_position.update(None)
+                self._exchange.cancel_all_orders(self._symbol)
+                logger.info(f"No active position for {symbol_info['id']}")
+                return
+            position = positions[0]
+            amount = position['contracts']
+            entry_price = position['entryPrice']
+            side = Side(position['side'])
+            my_position.update(side, amount, entry_price)
+            logger.info(
+                f"Current position:"
+                f" {amount * side.sign()} {symbol_info['base']}"
+                f" at {entry_price} {symbol_info['quote']}"
+            )
+        except Exception as e:
+            logger.error(f'Failed to fetch position: {e}')
             raise
 
     def fetch_account_balance(self, symbol_info: dict) -> float:
