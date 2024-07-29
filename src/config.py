67047aacaede3166ba6b7132212af7a09245cf01
Ki-jong Song <jjongs212@gmail.com>
2024-07-29 19:05:51 +0900

    Add type conversion method to Config class
    Convert environment variables to numbers if possible

--- src/config.py
+++ src/config.py
@@ -13,9 +13,19 @@ class Config:
 
     def __getattr__(self, name: str) -> Any:
         if attr := getenv(name):
-            return attr
+            return self._convert_type(attr)
         if name in self._config.get(self._env, {}):
             return self._config[self._env][name]
         if name in self._config['default']:
             return self._config['default'][name]
         raise AttributeError(f'{name} not set')
+
+    @staticmethod
+    def _convert_type(env: str) -> int | float | str:
+        try:
+            return int(env)
+        except ValueError:
+            try:
+                return float(env)
+            except ValueError:
+                return env
