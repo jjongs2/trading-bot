f71f3671f628688b885b2c8d6923bd34e4c26678
Ki-jong Song <jjongs212@gmail.com>
2024-07-29 13:30:27 +0900

    Create Config class

---
+++ src/config.py
@@ -0,0 +1,21 @@
+from os import getenv
+from typing import Any
+
+import yaml
+
+
+class Config:
+    def __init__(self) -> None:
+        self._env = getenv('ENVIRONMENT', 'production')
+        config_dir = '.' if self._env == 'production' else '..'
+        with open(f'{config_dir}/config.yaml', 'r') as f:
+            self._config = yaml.safe_load(f)
+
+    def __getattr__(self, name: str) -> Any:
+        if attr := getenv(name):
+            return attr
+        if name in self._config.get(self._env, {}):
+            return self._config[self._env][name]
+        if name in self._config['default']:
+            return self._config['default'][name]
+        raise AttributeError(f'{name} not set')
