e5f48340d8e77539eac346e74698975375581148
Ki-jong Song <jjongs212@gmail.com>
2024-07-29 16:39:40 +0900

    Implement AWS Lambda handler

--- src/lambda_function.py
+++ src/lambda_function.py
@@ -1,8 +1,41 @@
-import json
+from json import dumps
+from logging import getLogger
+
+import boto3
+import ccxt
+
+from config import Config
+from trader_factory import create_trader
+
+getLogger().setLevel('INFO')
+logger = getLogger(__name__)
+
 
 def lambda_handler(event, context):
-    # TODO implement
-    return {
-        'statusCode': 200,
-        'body': json.dumps('Hello from Lambda!')
-    }
+    logger.info('Starting lambda_handler function...')
+    try:
+        config = Config()
+        s3_client = boto3.client('s3')
+        exchange_class = getattr(ccxt, config.EXCHANGE_ID)
+        exchange = exchange_class(
+            {
+                'apiKey': config.EXCHANGE_API_KEY,
+                'secret': config.EXCHANGE_API_SECRET,
+                'options': {
+                    'maxRetriesOnFailure': config.MAX_RETRIES,
+                    'maxRetriesOnFailureDelay': config.RETRY_DELAY,
+                },
+            }
+        )
+        trader = create_trader(s3_client, exchange, config)
+        trader.execute_trade()
+        return {
+            'statusCode': 200,
+            'body': dumps('OK'),
+        }
+    except Exception as e:
+        logger.exception(f'Exception raised in lambda_handler: {e}')
+        return {
+            'statusCode': 500,
+            'body': dumps('Internal Server Error'),
+        }
